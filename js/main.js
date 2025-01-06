3c803bc6c4b59f0084e1690590bc0e98e28322e7
Lukas Velek <lukas.velek@centrum.cz>
2025-01-06 17:34:48 +0100

    Projects implementation 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
-// Set the copyright year
-$("#footer-text-date").html((new Date()).getFullYear());
+var isProjects = false;
+
+function init() {
+    // Set the copyright year
+    $("#footer-text-date").html((new Date()).getFullYear());
+    
+    if(isProjects) {
+        // Hide projects contents
+        closeModal("projects-skydocu-content");
+
+        $("#projects-skydocu").on("click", function() {
+            //$("#projects-skydocu-content").show();
+            openModal("projects-skydocu-content");
+        });
+    }
+}
+
+function closeModal(_name) {
+    $("#" + _name).hide();
+}
+
+function openModal(_name) {
+    $("#" + _name).show();
+}
