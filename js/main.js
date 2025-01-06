f79427308e3a4985e70c898af5fe9f1781cafad5
Lukas Velek <lukas.velek@centrum.cz>
2025-01-06 19:17:28 +0100

    Projects implementation 2

--- js/main.js
+++ js/main.js
@@ -5,12 +5,20 @@ function init() {
     $("#footer-text-date").html((new Date()).getFullYear());
     
     if(isProjects) {
-        // Hide projects contents
-        closeModal("projects-skydocu-content");
+        const projects = [
+            "skydocu"
+        ];
 
-        $("#projects-skydocu").on("click", function() {
-            //$("#projects-skydocu-content").show();
-            openModal("projects-skydocu-content");
+        // Hide projects contents
+        projects.forEach(function(value, index, array) {
+            const modalName = "projects-content-" + value;
+            const linkName = "projects-showcase-" + value;
+
+            closeModal(modalName);
+
+            $("#" + linkName).on("click", function() {
+                openModal(modalName);
+            });
         });
     }
 }
@@ -20,5 +28,14 @@ function closeModal(_name) {
 }
 
 function openModal(_name) {
-    $("#" + _name).show();
+    const name = "#" + _name;
+
+    $(name)
+        .show()
+        .css("position", "absolute")
+        .css("top", "10%")
+        .css("left", "10%")
+        .css("width", "80%")
+        .css("background-color", "rgb(200, 200, 200)")
+    ;
 }
